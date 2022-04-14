10290a545774cb8c7b1034e8434fd391fd2d3c89
Gabanelli <gabanelli.guilherme@gmail.com>
2022-04-13 22:42:44 -0300

    doc: Dart doc added

--- lib/src/reversible_bloc_mixin.dart
+++ lib/src/reversible_bloc_mixin.dart
@@ -2,22 +2,31 @@ import 'package:bloc/bloc.dart';
 
 import 'stack/stack.dart';
 
+/// A mixin that enables a `Bloc` or a `Cubit` to revert to previous
+/// state with `revert()` method.
+/// 
+/// Is highly recommended that you use `Equatable` states, because
+/// revert uses a comparision between states.
 mixin ReversibleBlocMixin<Event, State> on BlocBase<State> {
+
   late final Stack<State> _stateStack = Stack();
+  State? _isRevertingTo;
 
-  bool isReverting = false;
-
+  /// Reverts to a previous state
+  /// 
+  /// Throws [EmptyStackException] when the stack has
+  /// no previous states
   void revert() {
     final oldState = _stateStack.pop();
-    isReverting = true;
+    _isRevertingTo = oldState;
     emit(oldState);
   }
 
   @override
   void onChange(Change<State> change) {
     super.onChange(change);
-    if (isReverting) {
-      isReverting = false;
+    if (change.nextState == _isRevertingTo) {
+      _isRevertingTo = null;
     } else {
       _stateStack.push(change.currentState);
     }
--- lib/src/stack/stack.dart
+++ lib/src/stack/stack.dart
@@ -2,9 +2,11 @@ import 'dart:collection';
 
 import 'package:reversible_bloc/src/stack/empty_stack_exception.dart';
 
+/// Data structure used to stack elements
 class Stack<T> {
   final _storage = ListQueue<T>();
 
+  /// Get the last item of the stack, removing it
   T pop() {
     if (_storage.isEmpty) {
       throw const EmptyStackException();
@@ -12,9 +14,11 @@ class Stack<T> {
     return _storage.removeLast();
   }
 
+  /// Adds a new item in stack, on the last position
   void push(T element) {
     _storage.addLast(element);
   }
 
+  /// Get the stack length
   int get length => _storage.length;
 }
